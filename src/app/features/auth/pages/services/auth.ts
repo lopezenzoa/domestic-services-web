98dca7575a6c28f19537cdf8b4bf7667eec21d6a
agustina_1312 <agustina_1312@hotmail.com>
2025-10-15 21:04:44 -0300

    feat: agregar pantallas de login y registro con animaciones Lottie
    x
    X

---
+++ src/app/features/auth/pages/services/auth.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+}
