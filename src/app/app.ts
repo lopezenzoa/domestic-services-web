df28a588ed7bec4c193306fa47c6193cfb992f4c
agustina_1312 <agustina_1312@hotmail.com>
2025-10-19 20:53:34 -0300

    Agregue el navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Navbar } from "./shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet, CommonModule, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
