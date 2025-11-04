1e8db9b6c5d352e1c44755d3e30e5f7f1361539e
Ailen Martinez <ailenmartinezz22@gmail.com>
2025-11-04 11:30:38 -0300

    agregar estilos contrataciones

---
+++ src/app/features/providers/models/Call.ts
@@ -0,0 +1,9 @@
+export interface Call {
+    id?: number;
+    description: string;
+    date: string; 
+    address: string;
+    state: string;
+    clientId: number;
+    providerId: number;
+}
---
+++ src/app/features/providers/services/call-service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Call } from '../models/Call';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallService {
+  
+
+  url = 'http://localhost:8080/api/calls';
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  getCalls(providerId: number) : Observable<Call[]>{
+    return this.http.get<Call[]>(this.url + "/provider/" + providerId);
+  }
+}
