8cd34458d091eaa76bb7ecc40b89683c4d291b62
agustina_1312 <agustina_1312@hotmail.com>
2025-11-08 23:06:23 -0300

    Agregue el navbar y cambie el estilo (que hay que seguir corrigiendo)

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,34 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,CommonModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar implements OnInit{
-  menuOpen = false;
-  isLoggedIn = false;
-  userRole: 'CLIENTE'|'PRESTADOR'|'ADMIN'| null = null;
-  
-  constructor(private router: Router){}
+export class Navbar  {
 
-  ngOnInit() {
-    const user = localStorage.getItem('user');
-    if(user){
-      this.isLoggedIn = true;
-      this.userRole = JSON.parse(user).role;
-    }
-  }
-  toggleMenu(){
-    this.menuOpen = !this.menuOpen
-  }
-  logout(){
-    localStorage.removeItem('user');
-    this.isLoggedIn = false;
-    this.userRole = null;
-    this.router.navigate(['/auth/loggin'])
-  }
 }
